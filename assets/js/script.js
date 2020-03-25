1fc7420060fb9fbb320151f269e9eadeead65004
Vaidotas Skema <skema.vaidotas@gmail.com>
2020-03-25 23:09:39 +0200

    added css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
 
    $(this).toggleClass('completed');
 
 });
 
-$('span').click(function (event) {
+$('ul').on('click', 'span', function (event) {
     $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function (event) {
+    if(event.which === 13){
+
+        var todoText = $(this).val();
+        $(this).val("");
+
+        $('ul').append('<li><span><i class="fa fa-trash"></i> </span>' + todoText + '</li>');
+
+
+    }
+});
+
+$("h1").click(function () {
+    $('input[type="text"]').fadeToggle();
 });
